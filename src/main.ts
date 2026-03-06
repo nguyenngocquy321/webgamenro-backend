257ff121c4476d682f0df930d13f36affc036e0b
Nguyen Ngoc Quy <akashineok@gmail.com>
2026-03-06 20:28:00 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'https://webgamenro-backend.onrender.com',
+        origin: 'http://localhost:5173',
         credentials: true,
     });
     app.useGlobalPipes(
